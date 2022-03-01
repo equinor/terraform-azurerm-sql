189adeb528805e414be945347337a51cff83b1aa
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-03-01 10:56:52 +0100

    Update versions

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.94.0"
+      version = ">= 2.74.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.1.0"
+      version = ">= 3.0.0"
     }
   }
 }
