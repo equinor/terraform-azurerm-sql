2b334a49d000c8931de130dfe357013d76638edc
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-07-08 10:47:49 +0200

    Update versions

--- modules/database/versions.tf
+++ modules/database/versions.tf
@@ -4,12 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.2.0"
+      version = ">= 3.0.0" # todo(@hknutsen): find minimum required version
     }
   }
 }
