e213d32b952f10680f653207325b8a10e9a3a3a5
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-09-01 09:07:44 +0200

    chore: update test fixture

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -2,34 +2,24 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  application = random_id.this.hex
-  environment = "test"
-}
-
 data "azurerm_client_config" "current" {}
-
-data "http" "public_ip" {
-  url = "https://ifconfig.me"
-}
 
 resource "random_id" "this" {
   byte_length = 8
 }
 
 resource "azurerm_resource_group" "this" {
-  name     = "rg-${local.application}-${local.environment}"
+  name     = "${random_id.this.hex}-rg"
   location = var.location
 }
 
 module "sql" {
   source = "../.."
 
-  application          = local.application
-  environment          = local.environment
-  storage_account_name = "st${local.application}sql001"
-  location             = azurerm_resource_group.this.location
+  server_name          = "${random_id.this.hex}-sql"
   resource_group_name  = azurerm_resource_group.this.name
+  location             = azurerm_resource_group.this.location
+  storage_account_name = "${random_id.this.hex}sqlst"
 
   azuread_admin = {
     user_principal_name = "john.smith@example.com"
@@ -37,14 +27,15 @@ module "sql" {
   }
 
   firewall_rules = {
-    "AllowClientPublicIp" = [data.http.public_ip.body, data.http.public_ip.body]
+    "Rule1" = ["1.1.1.1", "1.1.1.1"],
+    "Rule2" = ["1.1.1.1", "1.1.1.1"],
+    "Rule3" = ["1.1.1.1", "1.1.1.1"]
   }
 }
 
 module "database" {
   source = "../../modules/database"
 
-  application = local.application
-  environment = local.environment
-  server_id   = module.sql.server_id
+  name      = "${random_id.this.hex}-sqldb"
+  server_id = module.sql.server_id
 }
