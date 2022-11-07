ffdc583a259c83c1dd26134f28c87d0b20a5fd93
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-11-07 11:19:16 +0100

    fix: remove admin creds for db module outputs

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -7,14 +7,3 @@ output "name" {
   description = "The name of this SQL database."
   value       = azurerm_mssql_database.this.name
 }
-
-output "admin_login" {
-  description = "The login username of the administrator of this SQL server."
-  value       = azurerm_mssql_server.this.administrator_login
-}
-
-output "admin_password" {
-  description = "The login password of the administrator of this SQL server."
-  value       = azurerm_mssql_server.this.administrator_login_password
-  sensitive   = true
-}
