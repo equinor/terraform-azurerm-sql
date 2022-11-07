75cde7ebd3f8625fdf8a15219c5c0e50f888bbd9
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-11-07 11:22:03 +0100

    refactor

--- outputs.tf
+++ outputs.tf
@@ -36,7 +36,7 @@ output "database_ids" {
   }
 }
 
-output "connection_strings_dotnet" {
+output "connection_strings" {
   description = "A mapping of database connection strings for this SQL Server."
   value = {
     for k, v in module.database : k => "Server=tcp:${azurerm_mssql_server.this.fully_qualified_domain_name},1433;Initial Catalog=${v.name};Persist Security Info=False;User ID=${azurerm_mssql_server.this.administrator_login};Password=${azurerm_mssql_server.this.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
