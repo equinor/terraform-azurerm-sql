e696d7cfabb4903e82fac68bb1e1f86fe255b6f1
Helena Solberg Kallekleiv <110530547+helenakallekleiv@users.noreply.github.com>
2023-01-10 14:48:56 +0100

    chore: remove resource_group block from complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = true  # The feature flag can be removed after clean-up.
-    }
-  }
+  features {}
 }
 
 locals {
@@ -29,15 +25,15 @@ module "sql" {
   # source = "github.com/equinor/terraform-azurerm-sql?ref=v0.0.0"
   source = "../.."
 
-  server_name                   = "sql-${random_id.this.hex}"
-  resource_group_name           = azurerm_resource_group.this.name
-  location                      = azurerm_resource_group.this.location
-  storage_account_name          = "st${random_id.this.hex}sql"
-  storage_container_name        = "st-container-${random_id.this.hex}-sql"
-  admin_login                   = "masterlogin"
-  azuread_admin_login           = "john.smith@example.com"
-  azuread_admin_object_id       = data.azurerm_client_config.current.object_id
-  firewall_azure_ips_allowed    = true
+  server_name                = "sql-${random_id.this.hex}"
+  resource_group_name        = azurerm_resource_group.this.name
+  location                   = azurerm_resource_group.this.location
+  storage_account_name       = "st${random_id.this.hex}sql"
+  storage_container_name     = "st-container-${random_id.this.hex}-sql"
+  admin_login                = "masterlogin"
+  azuread_admin_login        = "john.smith@example.com"
+  azuread_admin_object_id    = data.azurerm_client_config.current.object_id
+  firewall_azure_ips_allowed = true
 
   firewall_rules = {
     "office" = {
