81bb526eb4bc18f2783e180b0e05cc62c874a5dd
kov117 <126077239+kov117@users.noreply.github.com>
2023-03-22 10:27:39 +0100

    feat: [server] removed diagostic setting and log analytic

--- modules/server/main.tf
+++ modules/server/main.tf
@@ -119,25 +119,3 @@ resource "azurerm_mssql_server_vulnerability_assessment" "this" {
     emails                    = []
   }
 }
-
-resource "azurerm_monitor_diagnostic_setting" "this" {
-  name                           = var.diagnostic_setting_name
-  target_resource_id             = azurerm_mssql_server.this.id
-  log_analytics_workspace_id     = var.log_analytics_workspace_id
-  log_analytics_destination_type = var.log_analytics_destination_type
-
-  dynamic "enabled_log" {
-    for_each = toset(var.diagnostic_setting_enabled_log_categories)
-
-    content {
-      category = enabled_log.value
-    }
-  }
-  metric {
-    category = "AllMetrics"
-
-    retention_policy {
-      enabled = false
-    }
-  }
-}
