6795252b7c251ce1b0d4fe076d84f2e27c2b9db1
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-04-17 13:31:08 +0200

    refactor!: 1-to-1 mapping of module-to-resources (#77)
    Ensure 1-to-1 mapping of module calls to visible Azure resources.
    
    BREAKING CHANGE: submodule `database` must be explicitly called.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 [![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
 [![Equinor Terraform Baseline](https://img.shields.io/badge/Equinor%20Terraform%20Baseline-1.0.0-blueviolet)](https://github.com/equinor/terraform-baseline)
 
-Terraform module which creates an Azure SQL database.
+Terraform module which creates Azure SQL resources.
--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,30 @@
 output "server_id" {
   description = "The ID of this SQL Server."
-  value       = module.server.id
+  value       = azurerm_mssql_server.this.id
 }
 
 output "server_name" {
   description = "The name of this SQL Server."
-  value       = module.server.name
+  value       = azurerm_mssql_server.this.name
 }
 
 output "administrator_login" {
   description = "The login username of the administrator of this SQL server."
-  value       = module.server.administrator_login
+  value       = azurerm_mssql_server.this.administrator_login
 }
 
 output "administrator_password" {
   description = "The login password of the administrator of this SQL server."
-  value       = module.server.administrator_password
+  value       = azurerm_mssql_server.this.administrator_login_password
   sensitive   = true
 }
 
 output "storage_account_id" {
   description = "The ID of this Storage Account."
-  value       = module.server.storage_account_id
+  value       = azurerm_storage_account.this.id
 }
 
 output "storage_account_name" {
   description = "The name of this Storage Account."
-  value       = module.server.storage_account_name
-}
-
-output "database_id" {
-  description = "The ID of this SQL database."
-  value       = module.database.id
+  value       = azurerm_storage_account.this.name
 }
