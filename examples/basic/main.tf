bf594adbad2ddcaad3f139de7b887309357f2435
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-07-24 12:42:09 +0200

    chore: update examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -39,10 +39,14 @@ module "sql" {
   server_name                = "sql-${random_id.this.hex}"
   resource_group_name        = azurerm_resource_group.this.name
   location                   = azurerm_resource_group.this.location
-  administrator_login        = "masterlogin"
   log_analytics_workspace_id = module.log_analytics.workspace_id
   storage_account_id         = module.storage.account_id
   storage_blob_endpoint      = module.storage.blob_endpoint
+
+  azuread_administrator_login_username = "azureadadminlogin"
+  azuread_administrator_object_id      = data.azurerm_client_config.current.object_id
+
+  administrator_login = "sqladminlogin"
 }
 
 module "database" {
