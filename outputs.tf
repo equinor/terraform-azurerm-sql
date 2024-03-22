7f27a4363a6ccff45590cb2892b7ae236931c308
ErlendT <32453298+ErlendT@users.noreply.github.com>
2024-03-22 13:09:47 +0100

    feat: add server security policy and vulnerability assessment outputs (#137)
    * feat: Add outputs for security policy and assessment for baseline deployments
    
    * Update outputs.tf
    
    Co-authored-by: Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,13 @@ output "administrator_password" {
   value       = azurerm_mssql_server.this.administrator_login_password
   sensitive   = true
 }
+
+output "server_security_alert_policy_id" {
+  description = "The ID of this SQL server security alert policy."
+  value       = azurerm_mssql_server_security_alert_policy.this.id
+}
+
+output "server_vulnerability_assessment_id" {
+  description = "The ID of this SQL server vulnerability assessment."
+  value       = azurerm_mssql_server_vulnerability_assessment.this.id
+}
