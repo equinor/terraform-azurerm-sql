c4135049041d5946d0bdb33e9484200ae8a449f0
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-03-27 08:11:29 +0100

    chore: update variable and output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 output "server_id" {
-  description = "The ID of this SQL Server."
+  description = "The ID of this SQL server."
   value       = azurerm_mssql_server.this.id
 }
 
 output "server_name" {
-  description = "The name of this SQL Server."
+  description = "The name of this SQL server."
   value       = azurerm_mssql_server.this.name
 }
 
 output "administrator_login" {
-  description = "The login username of the administrator of this SQL server."
+  description = "The login username of the administrator for this SQL server."
   value       = azurerm_mssql_server.this.administrator_login
 }
 
 output "administrator_password" {
-  description = "The login password of the administrator of this SQL server."
+  description = "The login password of the administrator for this SQL server."
   value       = azurerm_mssql_server.this.administrator_login_password
   sensitive   = true
 }
 
 output "server_security_alert_policy_id" {
-  description = "The ID of this SQL server security alert policy."
+  description = "The ID of the security alert policy for this SQL server."
   value       = azurerm_mssql_server_security_alert_policy.this.id
 }
 
 output "server_vulnerability_assessment_id" {
-  description = "The ID of this SQL server vulnerability assessment."
+  description = "The ID of the vulnerability assessment for this SQL server."
   value       = azurerm_mssql_server_vulnerability_assessment.this.id
 }
