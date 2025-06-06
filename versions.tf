73f38896bac51982164f40daca3880f537b9452a
Helena Solberg Kallekleiv <110530547+helenakallekleiv@users.noreply.github.com>
2025-06-06 08:45:53 +0200

    chore: add comment explaining excluded Azure provider version v4.31 (#186)

--- versions.tf
+++ versions.tf
@@ -4,6 +4,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      # Exluding Azure provider version v4.31 due to a breaking change making optional arguments, only working in certain scenarios, required.
+      # Will keep this version excluded until fix is availiable. Ref.: https://github.com/hashicorp/terraform-provider-azurerm/pull/29789
       version = ">= 4.0.0, != 4.31.0"
     }
 
